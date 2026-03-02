6506835b985a98aa334e91ddaace01aad1352303
Naveen Prasanna <naveenprasannavnp@gmail.com>
2026-03-03 04:37:18 +0530

    Updated the code for JPA

---
+++ src/main/java/org/nit/jobapp/repo/JobRepo.java
@@ -0,0 +1,10 @@
+package org.nit.jobapp.repo;
+
+import org.nit.jobapp.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+}
